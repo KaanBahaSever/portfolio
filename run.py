cac6f69821d892452f85757e07171395c8ee63d0
kaan <kaanbahasever@gmail.com>
2025-02-12 14:47:12 +0300

    change server port from 5001 to 80 in run.py

--- run.py
+++ run.py
@@ -2,6 +2,6 @@ from kaan import app
 import os
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0",port=5001, threaded=True,debug=True)
+    app.run(host="0.0.0.0",port=80, threaded=True,debug=True)
 
 #ssh $DEPLOY_USER@$DEPLOY_SERVER 'bash /path/to/deploy/deploy_script.sh'

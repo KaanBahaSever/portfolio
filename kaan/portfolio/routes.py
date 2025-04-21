9c07a844e5da9b617c51c0536fcc6b16fcb763e9
Kaan Baha Sever <kaanbahasever@gmail.com>
2025-04-21 22:54:38 +0300

    refactor routes.py to rename hello_world function to home for clarity

--- kaan/portfolio/routes.py
+++ kaan/portfolio/routes.py
@@ -8,5 +8,17 @@ limiter = Limiter(get_remote_address, app=app)
 portfolio_bp = Blueprint('portfolio_bp', __name__)
 
 @portfolio_bp.route('/')
-def hello_world():
+def home():
     return render_template('welcome.html')
+
+@portfolio_bp.route('/portfolio')
+def about():
+    return 'about me'
+
+@portfolio_bp.route('/projects')
+def projects():
+    return 'projects'
+
+@portfolio_bp.route('/blog')
+def blog():
+    return 'blog'

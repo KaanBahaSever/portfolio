b7fbb2c23ef08e3dfd21cecd5e577dcc95aa175d
Kaan Baha Sever <kaanbahasever@gmail.com>
2025-04-27 23:28:17 +0300

    Add timestamp to home route for cache-busting in welcome.html

--- kaan/portfolio/routes.py
+++ kaan/portfolio/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 from kaan import app
+import datetime
 
 from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
@@ -9,7 +10,9 @@ portfolio_bp = Blueprint('portfolio_bp', __name__)
 
 @portfolio_bp.route('/')
 def home():
-    return render_template('welcome.html')
+    timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+    return render_template('welcome.html', timestamp=timestamp)
+
 
 @portfolio_bp.route('/portfolio')
 def about():
